355b754b45e9bc59bb1d6b4b0ca3ce4cd8a272f2
MishaHerscu <mishaherscu@gmail.com>
2016-07-28 12:00:19 -0400

    can delete and create list items

---
+++ app/components/listr-list/component.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  form: {
+    content: null,
+    done: false,
+  },
+  // clickItem is also a property here
+  actions: {
+    clickItem (item) {
+      this.sendAction('clickItem', item);
+    },
+    deleteItem (item) {
+      this.sendAction('deleteItem', item);
+    },
+    submit () {
+      let data = this.get('form');
+      data.list = this.get('list');
+      this.sendAction('createItem', data);
+      this.set('form.content', null);
+    },
+  },
+});
---
+++ app/components/listr-list/item/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'li',
+  classNameBindings: ['done'],
+
+  done: Ember.computed.alias('item.done'),
+
+  //click: references a function by its passed in name
+  actions: {
+    click () {
+      this.sendAction('click', this.get('item'));
+    },
+
+    delete () {
+      this.sendAction('delete', this.get('item'));
+    },
+  },
+
+  // store: Ember.inject.service('store'),
+  store: Ember.inject.service(),
+});
